88e8b089568c731c4c65ee667ef581f64797bc6e
Bob van der Valk <info@mawoo.nl>
2017-05-27 02:35:49 +0200

    init project

---
+++ src/main/java/nl/mawoo/smokesignal/SmokeSignal.java
@@ -0,0 +1,13 @@
+package nl.mawoo.smokesignal;
+
+/**
+ * Boot up the p2p application and listen for in and output
+ *
+ * @author Bob van der Valk
+ */
+public class SmokeSignal {
+
+    public static void main(String[] args) {
+
+    }
+}
