5c0a2033422824b5f158319768eba13620b8d5b3
Bob van der Valk <info@mawoo.nl>
2017-05-29 22:43:03 +0200

    Add message protocol class and unittest

--- src/main/java/nl/mawoo/smokesignal/protocol/MessageProtocol.java
+++ src/main/java/nl/mawoo/smokesignal/protocol/MessageProtocol.java
@@ -1,8 +1,8 @@
 package nl.mawoo.smokesignal.protocol;
 
-import nl.mawoo.smokesignal.protocol.models.MessageType;
-
-import java.util.Date;
+import com.google.gson.Gson;
+import nl.mawoo.smokesignal.protocol.models.MessageObject;
+import org.apache.log4j.Logger;
 
 /**
  * {message}
@@ -10,18 +10,15 @@ import java.util.Date;
  * @author Bob van der Valk
  */
 public class MessageProtocol {
-    private String type;
-    private MessageType messageType;
-    private Date  datetime;
+    private static Logger logger = Logger.getLogger(MessageProtocol.class);
+    private MessageObject messageObject;
 
-    public MessageProtocol(String type, MessageType messageType, Date datetime) {
-        this.type = type;
-        this.messageType = messageType;
-        this.datetime = datetime;
+    public MessageProtocol(MessageObject messageObject) {
+        this.messageObject = messageObject;
     }
 
     public String constructJsonMessage() {
-        
+        Gson gson = new Gson();
+        return gson.toJson(messageObject);
     }
-
 }
