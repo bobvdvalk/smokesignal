a2e1f4db8d7f95b1777f1ff9967b1dc9f1f488f2
Bob van der Valk <info@mawoo.nl>
2017-05-29 22:59:08 +0200

    finish outgoing message protocol

--- src/main/java/nl/mawoo/smokesignal/protocol/MessageProtocol.java
+++ src/main/java/nl/mawoo/smokesignal/protocol/MessageProtocol.java
@@ -2,7 +2,10 @@ package nl.mawoo.smokesignal.protocol;
 
 import com.google.gson.Gson;
 import nl.mawoo.smokesignal.protocol.models.MessageObject;
+import nl.mawoo.smokesignal.protocol.validation.MD5Generator;
 import org.apache.log4j.Logger;
+
+import java.security.NoSuchAlgorithmException;
 
 /**
  * {message}
@@ -17,7 +20,27 @@ public class MessageProtocol {
         this.messageObject = messageObject;
     }
 
-    public String constructJsonMessage() {
+    /**
+     * This method generates a message that can be send through the correct protocol
+     * @return String with md5~{json}
+     */
+    public String constructMessage() {
+        String json = constructJson();
+        MD5Generator md5Generator = new MD5Generator(json);
+        String md5 = null;
+        try {
+            md5 = md5Generator.generateMd5();
+        } catch (NoSuchAlgorithmException e) {
+            logger.error("Cannot parse json to md5", e);
+        }
+        return md5 +"~"+ json;
+    }
+
+    /**
+     * This class converts a messageObject to a json format.
+     * @return String in json format
+     */
+    public String constructJson() {
         Gson gson = new Gson();
         return gson.toJson(messageObject);
     }
